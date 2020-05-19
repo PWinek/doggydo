f49645309e885a34e811bcd3abd0bec3b9d7b3f4
PWinek <52742904+PWinek@users.noreply.github.com>
2020-05-19 18:32:03 +0200

    Add files via upload

---
+++ tasks/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),
+	path('logout/', views.logoutUser, name="logout"),
+	path('', views.index, name="list"),
+	path("update_task/<str:pk>/", views.updateTask, name="update_task"),
+	path("delete/<str:pk>/", views.deleteTask, name="delete")
+]
